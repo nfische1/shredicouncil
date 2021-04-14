021fa18cfc20b4495f70bad035165705f5d7a0b1
NuTasha Fischer <nutashafischer@NuTashas-MacBook-Pro.local>
2021-04-14 10:24:28 -0600

    second commit

---
+++ gator.js
@@ -0,0 +1,4 @@
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('Reptile:', process.env.reptile);
+console.log('Green:', process.env.green);
+console.log('Yellow:', process.env.yellow);
